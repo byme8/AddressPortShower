d8ff4a7289db8e08c5c3c000556968734fb0b115
Stanislav Silin <silin.stanislav@gmail.com>
2017-03-23 18:37:28 +0200

    Implement base functionality

--- AddressPortShower/Program.cs
+++ AddressPortShower/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Newtonsoft.Json;
 
 namespace AddressPortShower
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		class DeviceAddress
+		{
+			public DeviceAddress(IPEndPoint endPoint)
+			{
+				this.Address = endPoint.Address.ToString();
+				this.Port = endPoint.Port;
+			}
+
+			public int Port { get; private set; }
+			public string Address { get; private set; }
+		}
+
+		static void Main(string[] args)
+		{
+			Launch();
+			Console.ReadKey();
+		}
+
+		private static async void Launch()
+		{
+			var udpClient = new UdpClient(5000);
+
+			while (true)
+			{
+				var request = await udpClient.ReceiveAsync();
+				var message = JsonConvert.SerializeObject(
+					new DeviceAddress(request.RemoteEndPoint));
+
+				var bytes = Encoding.UTF8.GetBytes(message);
+
+				udpClient.SendAsync(bytes, bytes.Length, request.RemoteEndPoint);
+			}
+		}
+	}
 }
